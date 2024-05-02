e286055ef3e05daee8c498dad59c63f35568eb29
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2024-05-02 08:27:40 -0400

    Initial commit

---
+++ BinSearch.swift
@@ -0,0 +1,9 @@
+
+//  BinSearch.swift
+//
+//  Created by Kevin Csiffary
+//  Created on 2024-05-02
+//  Version 1.0
+//  Copyright (c) Kevin Csiffary. All rights reserved.
+//
+//  Change me daddy.
